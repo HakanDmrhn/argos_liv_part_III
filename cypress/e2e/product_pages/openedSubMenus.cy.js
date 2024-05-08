dad19b3f5f729ff025f8734ec587b8df8ecfac75
Hilal_B_P <hilal.posluoglu@hotmail.com>
2024-05-08 11:24:36 +0200

    Update openedSubMenus.cy.js

--- cypress/e2e/product_pages/openedSubMenus.cy.js
+++ cypress/e2e/product_pages/openedSubMenus.cy.js
@@ -13,7 +13,6 @@ describe('Integration test with visual testing - diverse submenus', function () 
 
 
         cy.get('#plissee-sub-menu > #colors > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Plissee-Menü: Plissee nach Farben')
         cy.argosScreenshot('Plissee-Menü: Plissee nach Farben', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
@@ -25,7 +24,6 @@ describe('Integration test with visual testing - diverse submenus', function () 
 
 
         cy.get('#plissee-sub-menu #rooms > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Plissee-Menü: Plissee für Räume')
         cy.argosScreenshot('Plissee-Menü: Plissee für Räume', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
@@ -37,7 +35,6 @@ describe('Integration test with visual testing - diverse submenus', function () 
 
 
         cy.get('#plissee-sub-menu #characteristics > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Plissee-Menü: Plissee nach Eigenschaften')
         cy.argosScreenshot('Plissee-Menü: Plissee nach Eigenschaften', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
@@ -49,7 +46,6 @@ describe('Integration test with visual testing - diverse submenus', function () 
 
 
         cy.get('#plissee-sub-menu #guide > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Plissee-Menü: Ratgeber')
         cy.argosScreenshot('Plissee-Menü: Ratgeber', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
@@ -68,7 +64,6 @@ describe('Integration test with visual testing - diverse submenus', function () 
 
 
         cy.get('#rollo-sub-menu > #colors > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Rollo-Menü: Rollo nach Farben')
         cy.argosScreenshot('Rollo-Menü: Rollo nach Farben', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
@@ -80,7 +75,6 @@ describe('Integration test with visual testing - diverse submenus', function () 
 
 
         cy.get('#rollo-sub-menu > #rooms > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Rollo-Menü: Rollo für Räume')
         cy.argosScreenshot('Rollo-Menü: Rollo für Räume', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
@@ -92,7 +86,6 @@ describe('Integration test with visual testing - diverse submenus', function () 
 
 
         cy.get('#rollo-sub-menu > #characteristics > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Rollo-Menü: Rollo nach Eigenschaften')
         cy.argosScreenshot('Rollo-Menü: Rollo nach Eigenschaften', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
@@ -104,7 +97,6 @@ describe('Integration test with visual testing - diverse submenus', function () 
 
 
         cy.get('#rollo-sub-menu > #guide > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Rollo-Menü: Ratgeber')
         cy.argosScreenshot('Rollo-Menü: Ratgeber', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
@@ -122,7 +114,6 @@ describe('Integration test with visual testing - diverse submenus', function () 
         cy.get('.nav-primary.clearfix > :nth-child(3)').click()
 
         cy.get('#doppelrollo-sub-menu > #colors > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Doppelrollo-Menü: Doppelrollo nach Farben')
         cy.argosScreenshot('Doppelrollo-Menü: Doppelrollo nach Farben', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
@@ -144,7 +135,6 @@ describe('Integration test with visual testing - diverse submenus', function () 
 
 
         cy.get('#vorhaenge-sub-menu > :nth-child(1) > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Vorhänge-Menü: Vorhänge')
         cy.argosScreenshot('Vorhänge-Menü: Vorhänge', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
@@ -153,7 +143,6 @@ describe('Integration test with visual testing - diverse submenus', function () 
         });
 
         cy.get('#vorhaenge-sub-menu > :nth-child(2) > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Vorhänge-Menü: Gardinen')
         cy.argosScreenshot('Vorhänge-Menü: Gardinen', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
@@ -162,7 +151,6 @@ describe('Integration test with visual testing - diverse submenus', function () 
         });
 
         cy.get('#vorhaenge-sub-menu > :nth-child(3) > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Vorhänge-Menü: Ösenschal')
         cy.argosScreenshot('Vorhänge-Menü: Ösenschal', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
@@ -171,16 +159,6 @@ describe('Integration test with visual testing - diverse submenus', function () 
         });
 
         cy.get('#vorhaenge-sub-menu > :nth-child(4) > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Vorhänge-Menü: Schlaufenschal')
-        cy.argosScreenshot('Vorhänge-Menü: Schlaufenschal', {
-            viewports: [
-                "iphone-6", // Use device preset for iphone-6
-                { width: 1280, height: 1024 }, // Specify dimensions directly
-            ]
-        });
-
-        cy.get('#vorhaenge-sub-menu > :nth-child(5) > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Vorhänge-Menü: Dekoschal')
         cy.argosScreenshot('Vorhänge-Menü: Dekoschal', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
@@ -188,8 +166,7 @@ describe('Integration test with visual testing - diverse submenus', function () 
             ]
         });
 
-        cy.get('#vorhaenge-sub-menu > :nth-child(8) > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Vorhänge-Menü: Zubehör')
+        cy.get('#vorhaenge-sub-menu > :nth-child(7) > .menu-wrapper').invoke('show')
         cy.argosScreenshot('Vorhänge-Menü: Zubehör', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
@@ -204,7 +181,6 @@ describe('Integration test with visual testing - diverse submenus', function () 
         cy.get('.nav-primary.clearfix > :nth-child(7)').click()
 
         cy.get('#schiebegardine-sub-menu > #colors > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Schiebegardinen-Menü: Schiebegardinen nach Farben')
         cy.argosScreenshot('Schiebegardinen-Menü: Schiebegardinen nach Farben', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
@@ -216,7 +192,6 @@ describe('Integration test with visual testing - diverse submenus', function () 
 
 
         cy.get('#schiebegardine-sub-menu > #characteristics > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Schiebegardinen-Menü: Schiebegardinen nach Eigenschaften')
         cy.argosScreenshot('Schiebegardinen-Menü: Schiebegardinen nach Eigenschaften', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
@@ -234,7 +209,6 @@ describe('Integration test with visual testing - diverse submenus', function () 
 
 
         cy.get('#raffrollo-sub-menu > #colors > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Raffrollo-Menü: Raffrollo nach Farben')
         cy.argosScreenshot('Raffrollo-Menü: Raffrollo nach Farben', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
@@ -246,7 +220,6 @@ describe('Integration test with visual testing - diverse submenus', function () 
 
 
         cy.get('#raffrollo-sub-menu > #rooms > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Raffrollo-Menü: Raffrollo für Räume')
         cy.argosScreenshot('Raffrollo-Menü: Raffrollo für Räume', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
@@ -258,7 +231,6 @@ describe('Integration test with visual testing - diverse submenus', function () 
 
 
         cy.get('#raffrollo-sub-menu > #characteristics > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Raffrollo-Menü: Raffrollo nach Eigenschaften')
         cy.argosScreenshot('Raffrollo-Menü: Raffrollo nach Eigenschaften', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
@@ -270,7 +242,6 @@ describe('Integration test with visual testing - diverse submenus', function () 
 
 
         cy.get('#raffrollo-sub-menu > #guide > .menu-wrapper').invoke('show')
-        // cy.percySnapshot('Raffrollo-Menü: Ratgeber')
         cy.argosScreenshot('Raffrollo-Menü: Ratgeber', {
             viewports: [
                 "iphone-6", // Use device preset for iphone-6
